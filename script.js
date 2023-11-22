db02abefd9c3477585b81eccd6308fc7db5bec95
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-22 22:58:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function stringChop(str, size) {
   // your code here
+	let str1="";
+	let arr=[];
+	for(let i=0; i<str.length; i++){
+		if(str1.length>=size){
+			arr.push(str1);
+			str1="";
+			str1+=str[i];
+		}else{
+			str1+=str[i];
+		}
+	}
+	arr.push(str1);
+	return arr;
 }
+
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
